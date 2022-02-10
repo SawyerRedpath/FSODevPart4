ff89ef460125925fa992f9d642e88cb8002f3e65
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2022-02-09 16:35:06 -0800

    4.8 post refactor of controller

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-    'jest': true,
-    'node': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+    node: true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
